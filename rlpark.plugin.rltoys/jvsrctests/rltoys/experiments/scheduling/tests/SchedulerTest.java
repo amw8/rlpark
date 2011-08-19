bf6c8e1f85140b393ee727c9f85fabebeed0923b
Thomas <degris@ualberta.ca>
2011-08-19 09:44:25 -0600

    Test with many iterators

--- rlpark.plugin.rltoys/jvsrctests/rltoys/experiments/scheduling/tests/SchedulerTest.java
+++ rlpark.plugin.rltoys/jvsrctests/rltoys/experiments/scheduling/tests/SchedulerTest.java
@@ -3,15 +3,22 @@ package rltoys.experiments.scheduling.tests;
 import static rltoys.experiments.scheduling.tests.SchedulerTestsUtils.testScheduler;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import junit.framework.Assert;
 
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 import rltoys.experiments.scheduling.network.NetworkClientScheduler;
 import rltoys.experiments.scheduling.network.ServerScheduler;
+import rltoys.experiments.scheduling.network.internal.LocalQueue;
 import rltoys.experiments.scheduling.network.internal.Messages;
 import rltoys.experiments.scheduling.network.internal.NetworkClassLoader;
 import rltoys.experiments.scheduling.schedulers.LocalScheduler;
+import rltoys.experiments.scheduling.tests.SchedulerTestsUtils.Job;
+import rltoys.experiments.scheduling.tests.SchedulerTestsUtils.JobDoneListener;
 
 public class SchedulerTest {
   private static final String Localhost = "localhost";
@@ -29,6 +36,26 @@ public class SchedulerTest {
   public void testJobScheduler() {
     LocalScheduler scheduler = new LocalScheduler(10);
     SchedulerTestsUtils.testScheduler(scheduler);
+    scheduler.dispose();
+  }
+
+  @Test(timeout = Timeout)
+  public void testJobSchedulerWithManyIterators() {
+    LocalScheduler scheduler = new LocalScheduler(10);
+    final int NbJobs = 100;
+    for (int i = 0; i < 2; i++) {
+      List<Job> jobs = SchedulerTestsUtils.createJobs(NbJobs);
+      SchedulerTestsUtils.assertAreDone(jobs, false);
+      JobDoneListener listener = SchedulerTestsUtils.createListener();
+      for (Job job : jobs) {
+        List<Runnable> oneElement = new ArrayList<Runnable>();
+        oneElement.add(job);
+        ((LocalQueue) scheduler.queue()).add(oneElement.iterator(), listener);
+      }
+      scheduler.runAll();
+      Assert.assertEquals(NbJobs, listener.nbJobDone());
+      SchedulerTestsUtils.assertAreDone(listener.jobDone(), true);
+    }
     scheduler.dispose();
   }
 
